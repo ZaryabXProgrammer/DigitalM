3aae203cced1cc48d52ce5a437c193b4f59523c1
Zaryab Haider <zaryabhaider88@gmail.com>
2024-08-15 14:39:06 +0500

    First Commit

---
+++ Javascript/index.js
@@ -0,0 +1,6 @@
+//Navbar
+
+document.querySelector(".toggleBtn").addEventListener("click", function () {
+  const centerMenu = document.querySelector(".center");
+  centerMenu.classList.toggle("active");
+});
